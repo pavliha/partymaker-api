48c0c37a60dad44898868655e98fb43c5268aff4
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-09-30 19:10:03 +0300

    fix comment create

--- app/Validators/Place/Comment/Store.js
+++ app/Validators/Place/Comment/Store.js
@@ -1,15 +1,5 @@
-const Comment = use('App/Models/Comment')
-
 module.exports = class Store {
 
-  async authorize() {
-    const { auth, response } = this.ctx
-    if (auth.user.cannot('create', Comment)) {
-      return response.forbidden()
-    }
-
-    return true
-  }
 
   get rules() {
     return {
