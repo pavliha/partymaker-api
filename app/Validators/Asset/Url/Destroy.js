d38d44a81e2d81b1f89c07591e9ac55e98eb7d54
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-10-06 14:04:31 +0300

    fix asset destroy

--- app/Validators/Asset/Url/Destroy.js
+++ app/Validators/Asset/Url/Destroy.js
@@ -1,11 +1,14 @@
 const Asset = use('App/Models/Asset')
+const Env = use('Env')
 
 module.exports = class Destroy {
 
   async authorize() {
-    this.ctx.asset = await Asset.findByOrFail({ url: this.ctx.params.url })
-    const { auth, response, place } = this.ctx
-    const can = auth.user.can('delete', place)
+    const fileName = this.ctx.params.url
+    const url = `${Env.get('APP_URL')}/uploads/${fileName}`
+    this.ctx.asset = await Asset.findByOrFail({ url })
+    const { auth, response } = this.ctx
+    const can = auth.user.can('delete', this.ctx.asset)
     if (!can) return response.forbidden()
     return true
   }
--- start/routes/asset.js
+++ start/routes/asset.js
@@ -1,12 +1,13 @@
 const Route = use('Route')
 
-Route.resource('assets', 'Asset/AssetController')
-  .validator([['assets.store', 'Asset/Store'], ['assets.destroy', 'Asset/Destroy']])
-  .middleware('auth')
-  .apiOnly()
-
 
 Route.get('assets/url', 'Asset/UrlController.index').middleware('auth')
 Route.get('assets/url/:url', 'Asset/UrlController.show').middleware('auth')
 Route.post('assets/url', 'Asset/UrlController.store').middleware('auth').validator('Asset/Url/Store')
 Route.delete('assets/url/:url', 'Asset/UrlController.destroy').middleware('auth').validator('Asset/Url/Destroy')
+
+
+Route.resource('assets', 'Asset/AssetController')
+  .validator([['assets.store', 'Asset/Store'], ['assets.destroy', 'Asset/Destroy']])
+  .middleware('auth')
+  .apiOnly()
