c3f00b0c975fe668eb9469c73d6fdf51395ae7dd
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-10-08 12:04:47 +0300

    add latest database dump

--- utils/processUrl.js
+++ utils/processUrl.js
@@ -3,7 +3,13 @@ const axios = require('axios')
 const Drive = use('Drive')
 
 const processUrl = async url => {
-  const name = `${new Date().getTime()}-${basename(url)}`
+  let name
+  try {
+    name = `${new Date().getTime()}-${basename(url)}`
+  } catch (error) {
+    name = `${new Date().getTime()}`
+  }
+
   const response = await axios.get(url, { responseType: 'arraybuffer' })
   const buffer = Buffer.from(response.data, 'base64')
   await Drive.put(name, buffer)
