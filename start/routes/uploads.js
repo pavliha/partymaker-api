6fb7d6508d7a76d5774694fa512c95022830aff0
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-11-17 02:52:49 +0200

    add uploads deletes

--- start/routes/uploads.js
+++ start/routes/uploads.js
@@ -1,5 +1,10 @@
 const Route = use('Route')
 
+
+Route.delete('/uploads/:filename', 'Uploads/UploadsController.destroy')
+  .middleware('auth')
+
+
 Route.post('/uploads/picture/file', 'Uploads/Picture/FileController.store')
   .middleware('auth')
 
