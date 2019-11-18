4a02a67f2fc64dbad964dea25379c7c6a606fa6b
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-11-18 13:58:30 +0200

    update build config

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/zsh
+
+echo 'Fetching new changes'
 git reset --hard origin/master
 git pull origin master
+
+echo 'Install dependencies'
 yarn install
+
+echo 'Copy new nginx config'
+sudo cp ./config.nginx /etc/nginx/sites-available/api.partymaker.zp.ua
+
+echo 'Restart nginx service'
+sudo service nginx restart
+
+echo 'Restart pm2'
+npm install -g pm2
 pm2 restart server.js
