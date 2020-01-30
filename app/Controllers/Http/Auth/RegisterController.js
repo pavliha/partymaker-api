5cae81475fe97d4e8f26c32f17483fc2b194e4ab
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2020-01-30 15:09:34 +0200

    fix user registration

--- app/Controllers/Http/Auth/RegisterController.js
+++ app/Controllers/Http/Auth/RegisterController.js
@@ -1,6 +1,6 @@
 const randomString = require('randomstring')
 const Token = use('App/Models/Token')
-const ActivationMail = use('App/Services/ActivationMail')
+// const ActivationMail = use('App/Services/ActivationMail')
 const User = use('App/Models/User')
 
 class RegisterController {
@@ -14,7 +14,7 @@ class RegisterController {
     const user = await User.create(fields)
     const token = randomString.generate()
     await user.tokens().create({ type: 'email', token })
-    ActivationMail.send({ user, token })
+    // ActivationMail.send({ user, token })
 
     return auth.withRefreshToken().generate(user, true)
   }
